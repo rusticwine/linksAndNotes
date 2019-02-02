5bd1a40e91b47ae4268d113692480d445589cf6f
rusticwine <Sisavusa1>
2019-02-02 17:24:10 +0100

    - initial import

---
+++ src/main/java/com/sisa/experiments/linksAndNotes/LinksAndNotesApplication.java
@@ -0,0 +1,14 @@
+package com.sisa.experiments.linksAndNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksAndNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksAndNotesApplication.class, args);
+	}
+
+}
+
